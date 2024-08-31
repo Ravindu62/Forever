1282009d8e749c7705940d1a23e9e29bf27bb51b
Ravindu Nanayakkara <ravindupasannanayakkara@gmail.com>
2024-08-31 09:37:55 +0530

    navbar completed, routes added to navigate withing the site

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,12 +9,14 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:pz-[9vw]'>
 
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
@@ -25,9 +27,8 @@ const App = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/place-order' component={<PlaceOrder />}/>
         <Route path='orders' component={<Orders/>}/>
-        
+
       </Routes>
-      
     </div>
   )
 }
